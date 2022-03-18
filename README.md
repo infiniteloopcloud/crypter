aa4ed0ce75718fbead39a7dff574f2b71753a94d
gyurielf <mail.gyorgy.kallai@gmail.com>
2022-03-18 23:06:37 +0100

    feat: typescript and svelte implementation

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+## Developing
+
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
 # crypter
 
 Crypter is a tool to encrypt/decrypt messages for secure chating
@@ -12,4 +33,4 @@ Two input field presented, one is the key, and the other is the message. The key
 
 #### Decrypt
 
-Present an input field and a key. The user need to send this key to the other participant who will use this key for the encryption. The output what you got from the other participant need to put into that single field. Click on the decrypt and the original message appers. 
+Present an input field and a key. The user need to send this key to the other participant who will use this key for the encryption. The output what you got from the other participant need to put into that single field. Click on the decrypt and the original message appers.
---
+++ src/routes/index.svelte
@@ -0,0 +1,191 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let globalKeys: Record<string, any>;
+
+	// Inputs
+	let encryptionKeyInput: string;
+	let plainTextInput: string;
+	let chiperTextInput: string;
+
+	// Outputs
+	let encryptionKeyOutput: string;
+	let plainTextOutput: string;
+	let chiperTextOutput: string;
+
+	const generateKey = async () => {
+		const keys = await window.crypto.subtle.generateKey(
+			{
+				name: 'RSA-OAEP',
+				modulusLength: 2048,
+				publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+				hash: { name: 'SHA-256' }
+			},
+			true,
+			['encrypt', 'decrypt']
+		);
+
+		const exportedPublicKey = await window.crypto.subtle.exportKey('jwk', keys.publicKey);
+		const exportedPrivateKey = await window.crypto.subtle.exportKey('jwk', keys.privateKey);
+
+		return {
+			keys,
+			exportedPublicKey: exportedPublicKey,
+			exportedPrivateKey: exportedPrivateKey,
+			encryptionKey: window.btoa(JSON.stringify(exportedPublicKey))
+		};
+	};
+
+	const encrypt = async () => {
+		const encryptionKeyObj = JSON.parse(window.atob(encryptionKeyInput));
+		const type = {
+			name: 'RSA-OAEP',
+			hash: 'SHA-256'
+		};
+
+		const encryptionKeyImported = await crypto.subtle.importKey(
+			'jwk',
+			encryptionKeyObj,
+			type,
+			true,
+			['encrypt']
+		);
+
+		const chipertext = await window.crypto.subtle.encrypt(
+			{ name: 'RSA-OAEP' },
+			encryptionKeyImported,
+			new TextEncoder().encode(plainTextInput)
+		);
+
+		const chipertextStr = String.fromCharCode.apply(null, new Uint8Array(chipertext));
+		const chipertextBase64 = window.btoa(chipertextStr);
+
+		// displayChipertext(chipertextBase64);
+		chiperTextOutput = chipertextBase64;
+	};
+
+	const decrypt = async () => {
+		const newChipertextStr = atob(chiperTextInput);
+		const chipertext = str2ab(newChipertextStr);
+
+		const decryptedAb = await window.crypto.subtle.decrypt(
+			{ name: 'RSA-OAEP' },
+			globalKeys.keys.privateKey,
+			chipertext
+		);
+
+		const decrypted = String.fromCharCode.apply(null, new Uint8Array(decryptedAb));
+
+		// displayPlaintext(decrypted);
+		plainTextOutput = decrypted;
+	};
+
+	const str2ab = (str: string) => {
+		var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+		var bufView = new Uint8Array(buf);
+		for (var i = 0, strLen = str.length; i < strLen; i++) {
+			bufView[i] = str.charCodeAt(i);
+		}
+		return buf;
+	};
+
+	const main = async () => {
+		const keys = await generateKey();
+		globalKeys = keys;
+		// displayEncryptionKey(keys.encryptionKey);
+		encryptionKeyOutput = keys.encryptionKey;
+	};
+
+	onMount(async () => {
+		await main();
+	});
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>Crypter</h1>
+
+	<h3>Encrypt</h3>
+	<form on:submit|preventDefault={encrypt}>
+		<label for="encryption_key">Encryption Key: </label>
+		<input
+			bind:value={encryptionKeyInput}
+			type="text"
+			id="encryption_key"
+			name="encryption_key"
+			class="mt-1
+            block
+            w-full
+            rounded-md
+            bg-gray-100
+            border-transparent
+            focus:border-gray-500 focus:bg-white focus:ring-0"
+		/>
+		<br />
+		<br />
+		<label for="plaintext">Plaintext:</label>
+		<textarea
+			bind:value={plainTextInput}
+			id="plaintext"
+			name="plaintext"
+			rows="4"
+			class="mt-1
+            block
+            w-full
+            rounded-md
+            bg-gray-100
+            border-transparent
+            focus:border-gray-500 focus:bg-white focus:ring-0"
+		/>
+		<br />
+		<br />
+		<button
+			type="submit"
+			class="px-4 py-2 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 transition-colors text-white rounded-md"
+		>
+			Encrypt
+		</button>
+	</form>
+
+	{#if chiperTextOutput}
+		<p id="chipertext-output" class="break-words">{chiperTextOutput}</p>
+	{/if}
+
+	<h3>Decrypt</h3>
+	{#if encryptionKeyOutput}
+		<p id="public-key" class="break-words">{encryptionKeyOutput}</p>
+	{/if}
+	<form on:submit|preventDefault={decrypt}>
+		<label for="chipertext">Chipertext:</label>
+		<input
+			bind:value={chiperTextInput}
+			type="text"
+			id="chipertext"
+			name="chipertext"
+			class="mt-1
+            block
+            w-full
+            rounded-md
+            bg-gray-100
+            border-transparent
+            focus:border-gray-500 focus:bg-white focus:ring-0"
+		/><br /><br />
+		<button
+			type="submit"
+			class="px-4 py-2 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 transition-colors text-white rounded-md"
+		>
+			Decrypt
+		</button>
+	</form>
+
+	{#if plainTextOutput}
+		<p id="plaintext-output break-words">{plainTextOutput}</p>
+	{/if}
+</section>
