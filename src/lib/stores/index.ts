0a78aeff7a9bba23addfb615c158481cbfdb695d
gyurielf <mail.gyorgy.kallai@gmail.com>
2022-04-23 09:35:33 +0200

    fix: cleaned up

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,15 +1,6 @@
-// import { persist, indexedDBStorage } from '@macfja/svelte-persistent-store';
 import { writable, type Writable } from 'svelte/store';
 // Types & Interfaces
-// import type { PersistentStore } from '@macfja/svelte-persistent-store';
 import type { KeyStore } from '$lib/models/interfaces/KeyStore.interface';
 
 // KeyStore
-// export const keyStore: PersistentStore<KeyStore | string> = persist(
-//     writable(''),
-//     indexedDBStorage(),
-//     'keyz'
-// );
-
-// KS
-export const keyStore: Writable<KeyStore | string> = writable('');
+export const keyStore: Writable<KeyStore> = writable();
